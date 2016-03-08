55d5f0ba76bafd068b05dc1eb8f10999abda757d
laura <lg3295@gmail.com>
2016-03-07 21:19:46 -1000

    3 more functions

--- functions.js
+++ functions.js
@@ -118,6 +118,9 @@ function calculate(operation, x, y) {
  * @return {boolean} `a` is larger than `b`
  */
 
+function isGreaterThan(a, b) {
+	return (a > b);
+}
 
 /**
  * Returns true if `a` is less than `b`.
@@ -126,14 +129,18 @@ function calculate(operation, x, y) {
  * @return {boolean} `a` is smaller than `b`
  */
 
-
+function isLessThan(a,b) {
+	return (a < b);
+}
 /**
  * Returns true if `a` and `b` are equal.
  * @param {number} a
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
-
+function areEqual(a, b) {
+	return (a === b);
+}
 
 /**
  * Returns the smallest value of two numbers.
@@ -142,28 +149,42 @@ function calculate(operation, x, y) {
  * @return {number} the smallest number
  */
 
-
+function minimum(x, y) {
+	if (x > y) {
+		return y;
+	}
+	return x;
+}
 /**
  * Returns the largest value of two numbers.
  * @param {number} x
  * @param {number} y
  * @return {number} the largest number
  */
-
+function maximum(a, b) {
+	if (a > b) {
+		return a;
+	}
+	return b;
+}
 
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
-
+function isEven(n) {
+	return (n % 2 === 0);
+}
 
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
-
+function isOdd(n) {
+	return (n % 2);
+}
 
 /**
  * Returns a letter grade.
@@ -177,6 +198,22 @@ function calculate(operation, x, y) {
  * @return {string} the score represented as a letter grade
  */
 
+function letterGrade(score, total) {
+	var percentage = Math.floor(score/total*100);
+
+	if (percentage > 89) {
+		return "A";
+	} else if (percentage > 79) {
+		return "B";
+	} else if (percentage > 69) {
+		return "C";
+	} else if (percentage > 59) {
+		return "D";
+	} else if (percentage > 0) {
+		return "F";
+	}
+}
+
 
 /**
  * Checks if a `restaurant` object has a `reviews` property.
@@ -185,7 +222,13 @@ function calculate(operation, x, y) {
  * @param {object} restaurant   represents a restaurant object
  * @return {object} restaurant
  */
-
+function incrementReviews(restaurant) {
+	if (restaurant.reviews !== undefined) {
+		return restaurant.reviews = restaurant.reviews + 1;
+	} else {
+		return restaurant.reviews = 1;
+	}
+}
 
 /**
  * Joins two strings with a space.
@@ -193,7 +236,9 @@ function calculate(operation, x, y) {
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
-
+function combine(word1, word2) {
+	return word1.concat(word2);
+}
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
